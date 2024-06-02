278dd82ccf828ba1e5f057971e2e7ab0e8c761b8
husky <husky.robot.dog@gmail.com>
2024-06-03 00:06:37 +0800

    coding

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,26 @@
 use anyhow::Result;
 
-pub fn init<T, DK, DV>(name: Option<&str>, defaults: Vec<(DK, DV)>) -> Result<T>
+pub fn init<T, D, DK, DV>(name: Option<&str>, defaults: D) -> Result<T>
 where
-    T: Sized + for<'a> serde::Deserialize<'a>,
-    DK: AsRef<str>,
-    DV: Into<config::Value>,
+	T: Sized + for<'a> serde::Deserialize<'a>,
+	D: IntoIterator<Item = (DK, DV)>,
+	DK: AsRef<str>,
+	DV: Into<config::Value>,
 {
-    let mut settings = config::Config::builder()
-        .add_source(config::File::with_name(name.unwrap_or("config")).required(false))
-        .add_source(
-            config::Environment::with_prefix("APP")
-                .try_parsing(true)
-                .separator("_")
-                .list_separator(" "),
-        );
+	let mut settings = config::Config::builder()
+		.add_source(config::File::with_name(name.unwrap_or("config")).required(false))
+		.add_source(
+			config::Environment::with_prefix("APP")
+				.try_parsing(true)
+				.separator("_")
+				.list_separator(" "),
+		);
 
-    for (key, value) in defaults {
-        settings = settings.set_default(key, value)?;
-    }
+	for (key, value) in defaults {
+		settings = settings.set_default(key, value)?;
+	}
 
-    let settings = settings.build()?;
+	let settings = settings.build()?;
 
-    Ok(settings.try_deserialize()?)
+	Ok(settings.try_deserialize()?)
 }
